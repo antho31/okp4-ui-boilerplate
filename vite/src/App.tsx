51d2b5f474e52e1947f07f91b799f11047321ec7
Anthony Gourraud <10244943+antho31@users.noreply.github.com>
2023-09-19 17:00:19 +0200

    feat(vite): add basic connection component

--- vite/src/App.tsx
+++ vite/src/App.tsx
@@ -1,35 +1,22 @@
+import { configureGraz } from "graz";
+
+import { Connection } from "./components";
+
 import "./App.css";
-import { configureGraz, useActiveChain } from "graz";
+
+import { OKP4TestnetChain } from "./constants";
 
 configureGraz({
-    defaultChain: {
-        chainId: "okp4-nemeton-1",
-        gas: {
-            price: "10000000",
-            denom: "uknow",
-        },
-        currencies: [
-            {
-                coinDenom: "know",
-                coinMinimalDenom: "uknow",
-                coinDecimals: 6,
-                coinGeckoId: "OKP4 nemeton",
-                coinImageUrl:
-                    "https://raw.githubusercontent.com/cosmos/chain-registry/master/testnets/okp4testnet/images/okp4.png",
-            },
-        ],
-        rpc: "https://api.testnet.okp4.network:443/rpc",
-        rest: "https://api.testnet.okp4.network:443/",
-    },
+    defaultChain: OKP4TestnetChain,
 });
 
 function App() {
-    const activeChain = useActiveChain();
-
     return (
-        <div>
-            <span>Connected to {activeChain?.chainId}</span>
-        </div>
+        <>
+            <h1>OKP4 Vite Starter</h1>
+
+            <Connection chainInfo={OKP4TestnetChain} />
+        </>
     );
 }
 
