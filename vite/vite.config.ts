cc1acbf9f83b06dd0194eaf35ee4ca09ccbe0877
Anthony Gourraud <10244943+antho31@users.noreply.github.com>
2023-09-19 16:40:39 +0200

    feat(vite): add nodejs polyfills

--- vite/vite.config.ts
+++ vite/vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import nodePolyfills from "vite-plugin-node-stdlib-browser";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [nodePolyfills(), react()],
+});
