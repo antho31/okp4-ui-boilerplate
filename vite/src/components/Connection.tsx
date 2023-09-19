c895d35c0c9bd5aa0b87542ffe67769322093620
Anthony Gourraud <10244943+antho31@users.noreply.github.com>
2023-09-19 17:05:42 +0200

    feat(vite): add multiple wallet support for connection

--- vite/src/components/Connection.tsx
+++ vite/src/components/Connection.tsx
@@ -1,4 +1,5 @@
 import {
+    getAvailableWallets,
     useAccount,
     useDisconnect,
     useSuggestChainAndConnect,
@@ -7,6 +8,7 @@ import {
 import { ChainInfo, Key } from "graz/dist/keplr";
 
 export function Connection({ chainInfo }: { chainInfo: ChainInfo }) {
+    const wallets = getAvailableWallets();
     const { suggestAndConnect } = useSuggestChainAndConnect();
     const { disconnect } = useDisconnect();
     const {
@@ -28,17 +30,111 @@ export function Connection({ chainInfo }: { chainInfo: ChainInfo }) {
                 </>
             ) : (
                 <>
-                    <button
-                        onClick={() =>
-                            suggestAndConnect({
-                                chainInfo,
-                                walletType: WalletType.KEPLR,
-                                gas,
-                            })
-                        }
-                    >
-                        Connect with Keplr
-                    </button>
+                    {wallets.keplr && (
+                        <button
+                            onClick={() =>
+                                suggestAndConnect({
+                                    chainInfo,
+                                    walletType: WalletType.KEPLR,
+                                    gas,
+                                })
+                            }
+                        >
+                            Connect with Keplr
+                        </button>
+                    )}
+                    {wallets.leap && (
+                        <button
+                            onClick={() =>
+                                suggestAndConnect({
+                                    chainInfo,
+                                    walletType: WalletType.LEAP,
+                                    gas,
+                                })
+                            }
+                        >
+                            Connect with Leap
+                        </button>
+                    )}
+                    {wallets.cosmostation && (
+                        <button
+                            onClick={() =>
+                                suggestAndConnect({
+                                    chainInfo,
+                                    walletType: WalletType.COSMOSTATION,
+                                    gas,
+                                })
+                            }
+                        >
+                            Connect with Cosmostation
+                        </button>
+                    )}
+                    {wallets.walletconnect && (
+                        <button
+                            onClick={() =>
+                                suggestAndConnect({
+                                    chainInfo,
+                                    walletType: WalletType.WALLETCONNECT,
+                                    gas,
+                                })
+                            }
+                        >
+                            Connect with WalletConnect
+                        </button>
+                    )}
+                    {wallets.wc_keplr_mobile && (
+                        <button
+                            onClick={() =>
+                                suggestAndConnect({
+                                    chainInfo,
+                                    walletType: WalletType.WC_KEPLR_MOBILE,
+                                    gas,
+                                })
+                            }
+                        >
+                            Connect with Keplr Mobile
+                        </button>
+                    )}
+                    {wallets.wc_leap_mobile && (
+                        <button
+                            onClick={() =>
+                                suggestAndConnect({
+                                    chainInfo,
+                                    walletType: WalletType.WC_LEAP_MOBILE,
+                                    gas,
+                                })
+                            }
+                        >
+                            Connect with Leap Mobile
+                        </button>
+                    )}
+                    {wallets.wc_cosmostation_mobile && (
+                        <button
+                            onClick={() =>
+                                suggestAndConnect({
+                                    chainInfo,
+                                    walletType:
+                                        WalletType.WC_COSMOSTATION_MOBILE,
+                                    gas,
+                                })
+                            }
+                        >
+                            Connect with Cosmostation Mobile
+                        </button>
+                    )}
+                    {wallets.metamask_snap_leap && (
+                        <button
+                            onClick={() =>
+                                suggestAndConnect({
+                                    chainInfo,
+                                    walletType: WalletType.METAMASK_SNAP_LEAP,
+                                    gas,
+                                })
+                            }
+                        >
+                            Connect with Metamask
+                        </button>
+                    )}
                 </>
             )}
         </div>
