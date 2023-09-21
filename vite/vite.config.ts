e6be4cd01904edc7d664acb6a58baa2d898dca5e
Anthony Gourraud <10244943+antho31@users.noreply.github.com>
2023-09-21 14:14:24 +0200

    fix(config): add autoprefixer for postcss

--- vite/vite.config.ts
+++ vite/vite.config.ts
@@ -1,4 +1,6 @@
 import { defineConfig } from "vite";
+
+import autoprefixer from "autoprefixer";
 import nodePolyfills from "vite-plugin-node-stdlib-browser";
 import react from "@vitejs/plugin-react-swc";
 
@@ -6,8 +8,8 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
     plugins: [nodePolyfills(), react()],
     css: {
-    postcss: {
-      plugins: [autoprefixer({})]
-    }
-  },
+        postcss: {
+            plugins: [autoprefixer({})],
+        },
+    },
 });
